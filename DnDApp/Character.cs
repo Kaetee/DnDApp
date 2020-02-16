fa75839a03c73f5d6d7253fe9172de87c2f26849
Kai Odenix <marceltroscianko@gmail.com>
2020-02-16 02:44:41 +0000

    initial commit

---
+++ DnDApp/Character.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDApp {
+    enum EnumAttributes {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+
+    enum EnumCoins {
+        Copper,
+        Silver,
+        Gold,
+        Electrum,
+        Platinum
+    }
+
+    enum EnumSkills {
+        Acrobatics,
+        AnimalHandling,
+        Arcana,
+        Athletics,
+        Deception,
+        History,
+        Insight,
+        Intimidation,
+        Investigation,
+        Medicine,
+        Nature,
+        Perception,
+        Performance,
+        Persuation,
+        Religion,
+        SleightOfHand,
+        Stealth,
+        Survival
+    }
+
+    struct HitPoints {
+        short maximum;
+        short current;
+        short temporary;
+
+        HitPoints(short m, short c, short t) {
+            maximum = m;
+            current = c;
+            temporary = t;
+        }
+
+        HitPoints(short m) : this(m, m, 0) { }
+
+        void Damage(short points) {
+            current -= points;
+        }
+
+        void Heal(short points, bool overheal = false) {
+            if (overheal) {
+                short leftover = (short)(points - (maximum - current));
+
+                if (leftover > 0)
+                    temporary += leftover;
+            }
+
+            current += points;
+            LimitCurrent();
+        }
+
+        void LimitCurrent() {
+            current = (current > maximum) ? maximum : current;
+        }
+
+        void Reset(bool fullReset = false) {
+            current = maximum;
+
+            if (fullReset)
+                temporary = 0;
+        }
+    }
+
+    struct HitDice {
+        short count;
+        short sides;
+        short remaining;
+
+        HitDice(short c, short s, short r) {
+            count = c;
+            sides = s;
+            remaining = r;
+        }
+
+        HitDice(short c, short s) : this(c, s, c) { }
+
+        void Reset() { remaining = count; }
+    }
+
+    struct DeathSaves {
+        int successes;
+        int failures;
+
+        public DeathSaves(int s, int f) {
+            successes = s;
+            failures = f;
+        }
+
+        public int Successes {
+            get { return successes; }
+        }
+
+        public int Failures {
+            get { return failures; }
+        }
+
+        public void IncrementSuccesses() {
+            successes += (successes == 3) ? 0 : 1;
+        }
+
+        public void DecrementSuccesses() {
+            successes -= (successes == 0) ? 0 : 1;
+        }
+
+        public void IncrementFailures() {
+            failures += (failures == 3) ? 0 : 1;
+        }
+
+        public void DecrementFailures() {
+            failures -= (failures == 0) ? 0 : 1;
+        }
+
+        public void ResetSuccesses() { successes = 0; }
+        public void ResetFailures() { failures = 0; }
+        public void Reset() { ResetSuccesses(); ResetFailures(); }
+    }
+
+    partial class Purse {
+        int[] coins;
+
+        Purse() {
+            coins = new int[5];
+        }
+
+        Purse(int[] oldCoins) {
+            coins = oldCoins;
+            Recalculate();
+        }
+
+        Purse(int cp, int sp ,int gp, int ep, int pp) {
+            coins = new int[] { cp, sp, gp, ep, pp };
+            Recalculate();
+        }
+
+        int Copper {
+            get { return coins[(int)EnumCoins.Copper]; }
+            set {
+                coins[(int)EnumCoins.Copper] = value;
+                Recalculate();
+            }
+        }
+
+        int Silver {
+            get { return coins[(int)EnumCoins.Silver]; }
+            set {
+                coins[(int)EnumCoins.Silver] = value;
+                Recalculate();
+            }
+        }
+
+        int Gold {
+            get { return coins[(int)EnumCoins.Gold]; }
+            set {
+                coins[(int)EnumCoins.Gold] = value;
+                Recalculate();
+            }
+        }
+
+        int Electrum {
+            get { return coins[(int)EnumCoins.Electrum]; }
+            set {
+                coins[(int)EnumCoins.Electrum] = value;
+                Recalculate();
+            }
+        }
+
+        int Platinum {
+            get { return coins[(int)EnumCoins.Platinum]; }
+            set {
+                coins[(int)EnumCoins.Platinum] = value;
+                Recalculate();
+            }
+        }
+
+        void Recalculate() {
+            int remainder;
+            int carrier;
+
+            for (int i = 0; i < coins.Length - 1; i++) {
+                remainder = coins[i] % 100;
+                carrier = (coins[i] - remainder) / 100;
+                coins[i] = remainder;
+                coins[i + 1] += carrier;
+            }
+        }
+    }
+
+    struct Item {
+        string name;
+        string description;
+    }
+
+    class Character {
+        short[] attributes = new short[6];
+        short[] savingThrows = new short[6];
+        short[] skills = new short[18];
+        bool inspiration;
+        bool proficiency;
+
+        short armourClass;
+        short initiative;
+        short speed;
+        HitPoints hitPoints;
+        DeathSaves deathSaves;
+
+        short[] activeAttacks = new short[] { -1, -1, -1 };
+        
+
+        Character(HitPoints hp) {
+            for (int i = 0; i < attributes.Length; i++) {
+                attributes[i] = 0;
+            }
+
+            for (int i = 0; i < skills.Length; i++) {
+                skills[i] = 0;
+            }
+
+            hitPoints = hp;
+        }
+    }
+}
---
+++ DnDApp/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DnDApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        struct DeathSaves {
+            int successes;
+            int failures;
+
+            public DeathSaves(int s, int f) {
+                successes = s;
+                failures = f;
+            }
+
+            public int Successes {
+                get { return successes; }
+            }
+
+            public int Failures {
+                get { return failures; }
+            }
+
+            public void IncrementSuccesses() {
+                successes += (successes == 3) ? 0 : 1;
+            }
+
+            public void DecrementSuccesses() {
+                successes -= (successes == 0) ? 0 : 1;
+            }
+
+            public void IncrementFailures() {
+                failures += (failures == 3) ? 0 : 1;
+            }
+
+            public void DecrementFailures() {
+                failures -= (failures == 0) ? 0 : 1;
+            }
+        }
+
+        DeathSaves deathSaves;
+        bool inspiration;
+
+        public MainWindow() {
+            InitializeComponent();
+            deathSaves = new DeathSaves(0, 0);
+            inspiration = false;
+        }
+
+        private void DiceHitCountUpClick(object sender, RoutedEventArgs e) {
+            int diceCount;
+            if (Int32.TryParse(dice_hit_count.Text, out diceCount)) {
+                dice_hit_count.Text = (diceCount + 1).ToString();
+            }
+        }
+
+        private void DiceHitCountDownClick(object sender, RoutedEventArgs e) {
+            int diceCount;
+            if (Int32.TryParse(dice_hit_count.Text, out diceCount)) {
+                diceCount--;
+                dice_hit_count.Text = (diceCount < 0 ? 0 : diceCount).ToString();
+            }
+        }
+
+        private void DiceHitSizeUpClick(object sender, RoutedEventArgs e) {
+            int diceSize;
+            if (Int32.TryParse(dice_hit_size.Text, out diceSize)) {
+                dice_hit_size.Text = (diceSize + 1).ToString();
+            }
+        }
+
+        private void DiceHitSizeDownClick(object sender, RoutedEventArgs e) {
+            int diceSize;
+            if (Int32.TryParse(dice_hit_size.Text, out diceSize)) {
+                diceSize--;
+                dice_hit_size.Text = (diceSize < 1 ? 1 : diceSize).ToString();
+            }
+        }
+
+        private void DiceHitCurrentDown(object sender, RoutedEventArgs e) {
+            int diceCurrent;
+            if (Int32.TryParse(dice_hit_current.Text, out diceCurrent)) {
+                diceCurrent--;
+                dice_hit_current.Text = (diceCurrent < 0 ? 0 : diceCurrent).ToString();
+            }
+        }
+
+        private void DiceHitCurrentUp(object sender, RoutedEventArgs e) {
+            int diceCurrent;
+            if (Int32.TryParse(dice_hit_current.Text, out diceCurrent)) {
+                dice_hit_current.Text = (diceCurrent + 1).ToString();
+            }
+        }
+
+        private void DrawSuccesses() {
+            if (deathSaves.Successes >= 1)
+                death_save_success_0.Visibility = Visibility.Visible;
+            else
+                death_save_success_0.Visibility = Visibility.Hidden;
+
+            if (deathSaves.Successes >= 2)
+                death_save_success_1.Visibility = Visibility.Visible;
+            else
+                death_save_success_1.Visibility = Visibility.Hidden;
+
+            if (deathSaves.Successes >= 3)
+                death_save_success_2.Visibility = Visibility.Visible;
+            else
+                death_save_success_2.Visibility = Visibility.Hidden;
+        }
+
+        private void DrawFailures() {
+            if (deathSaves.Failures >= 1)
+                death_save_fail_0.Visibility = Visibility.Visible;
+            else
+                death_save_fail_0.Visibility = Visibility.Hidden;
+
+            if (deathSaves.Failures >= 2)
+                death_save_fail_1.Visibility = Visibility.Visible;
+            else
+                death_save_fail_1.Visibility = Visibility.Hidden;
+
+            if (deathSaves.Failures >= 3)
+                death_save_fail_2.Visibility = Visibility.Visible;
+            else
+                death_save_fail_2.Visibility = Visibility.Hidden;
+        }
+
+        private void DeathSavesSuccessDown(object sender, RoutedEventArgs e) {
+            deathSaves.DecrementSuccesses();
+            DrawSuccesses();
+        }
+
+        private void DeathSavesSuccessUp(object sender, RoutedEventArgs e) {
+            deathSaves.IncrementSuccesses();
+            DrawSuccesses();
+        }
+
+        private void DeathSavesFailDown(object sender, RoutedEventArgs e) {
+            deathSaves.DecrementFailures();
+            DrawFailures();
+        }
+
+        private void DeathSavesFailUp(object sender, RoutedEventArgs e) {
+            deathSaves.IncrementFailures();
+            DrawFailures();
+        }
+
+        private void ToggleInspiration(object sender, RoutedEventArgs e) {
+            inspiration = !inspiration;
+
+            if (inspiration)
+                inspirationBox.Visibility = Visibility.Visible;
+            else
+                inspirationBox.Visibility = Visibility.Hidden;
+        }
+    }
+}
